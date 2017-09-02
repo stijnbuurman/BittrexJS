a5310a93d323f1694765c3498081a24b409ba19a
stijnbuurman <stijn@dutchmedialab.nl>
2017-09-02 18:28:17 +0200

    Remove credentials.. the key has been disabled and was read only anyhow.

--- index.ts
+++ index.ts
@@ -6,11 +6,9 @@ import {BittrexAPI} from './src/BittrexAPI';
 let bittrexApi = new BittrexAPI();
 
 bittrexApi.authenticate({
-    apikey: '55b23e980dc349369318df8ded1ebb55',
-    apisecret: '15e54c020e3943ffb4307cbe7e1c20e2'
+    apikey: '',
+    apisecret: '',
 });
-
-console.log('orderid');
 
 bittrexApi.getOrder('d3686855-fa6a-45d9-8e6b-44476018fe52')
     .then((response) => {
